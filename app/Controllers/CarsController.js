8c72176a07912eac07013f6d4f35b0529e1add8b
Jake Overall <joverall22@gmail.com>
2023-02-01 11:36:19 -0700

    testing form submti

--- app/Controllers/CarsController.js
+++ app/Controllers/CarsController.js
@@ -1,3 +1,5 @@
+import { getFormData } from "../Utils/FormHandler.js"
+import { Pop } from "../Utils/Pop.js"
 import { setText } from "../Utils/Writer.js"
 
 export class CarsController {
@@ -6,9 +8,20 @@ export class CarsController {
     this.show()
   }
 
-  show(){
+  show() {
     setText('add-listing-button', '🚙 A new Car?')
     setText('listingFormLabel', '🚓 Impounded title... Get it cheap')
+  }
+
+  handleFormSubmit() {
+    try {
+      event.preventDefault()
+      const form = event.target
+      const formData = getFormData(form)
+      console.log(formData)
+    } catch (error) {
+      Pop.error(error)
+    }
   }
 
 }
